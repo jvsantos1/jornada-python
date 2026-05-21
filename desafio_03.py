5d8320cba666b59885ac1c07aff3efa33575729f
João Victor <joao.osantos27@gmail.com>
2026-05-21 19:52:30 -0300

    Adicionando o type hint nas variáveis

--- desafio_03.py
+++ desafio_03.py
@@ -2,14 +2,14 @@
 # que repita o fluxo até que o usuário insira as 
 # informações corretas
 
-CONSTANTE_BONUS = 1000
-validacao = True
+CONSTANTE_BONUS: int = 1000
+validacao: bool = True
 
 # Solicita ao usuário que digite seu nome
 
 while validacao:
     try:
-        nome = input("Digite seu nome: ")
+        nome: str = input("Digite seu nome: ")
         if nome.isdigit() or nome == "" or nome.isspace():
             print("Nome inválido! Digite um nome válido")
         else: break
@@ -19,7 +19,7 @@ while validacao:
 # Solicita ao usuário que digite o valor do seu salário e converte para float
 while validacao:
     try:
-        salario = float(input("Digite seu salario: ").replace(",", "."))
+        salario: float = float(input("Digite seu salario: ").replace(",", "."))
         if salario < 0:
             print("Valor do salário inválido! Digite um valor válido")
         else: break
@@ -29,7 +29,7 @@ while validacao:
 # Solicita ao usuário que digite o valor do bônus recebido e converte para float
 while validacao:
     try:
-        bonus = float(input("Digite seu bônus: ").replace(",", "."))
+        bonus: float = float(input("Digite seu bônus: ").replace(",", "."))
         if bonus < 0:
             print("Valor do bônus inválido! Digite novamente")
         else: break
@@ -37,7 +37,7 @@ while validacao:
         print(e)
 
 # Imprime as informações para o usuário
-calc_kpi = CONSTANTE_BONUS + float(salario) * float(bonus)
+calc_kpi: float = CONSTANTE_BONUS + salario * bonus
 print(
-    f"Olá {nome} o valor do seu salário é {float(salario)} reais e seu bonus foi de {float(bonus)} reais e o seu kpi é de {calc_kpi} reais"
+    f"Olá {nome} o valor do seu salário é {float(salario)} reais e seu bonus foi de {float(bonus)} reais e o seu kpi é de {float(calc_kpi)} reais"
 )
